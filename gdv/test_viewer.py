97da6fef2d518eaa1d506fc27c17281d0f0a0649
Justus Karlsson <karlssonjustus@gmail.com>
2024-02-10 23:59:41 +0100

    moderngl - render a cow mesh

--- gdv/test_viewer.py
+++ gdv/test_viewer.py
@@ -1,36 +1,29 @@
-import OpenGL
+import moderngl_window as mglw
+from pathlib import Path
+import numpy as np
+from pyrr import Matrix44
+import moderngl
 
-from OpenGL.GL import *
-from OpenGL.GLU import *
+class Test(mglw.WindowConfig):
+    gl_version = (3, 3)
+    window_size = (1920, 1080)
+    aspect_ratio = 16 / 9
+    title = "My Config"
+    samples = 8
+    resizable = False
+    resource_dir = (Path(__file__).parent / '../data').resolve()
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cow_scene = self.load_scene('cow/cow.obj')
+        
 
+    def render(self, time, frametime):
+        self.ctx.clear(0.0, 0.0, 0.0, 0.0)
+        self.ctx.enable(moderngl.DEPTH_TEST)
+        camera = np.eye(4)
+        projection = np.eye(4)
+        # projection = Matrix44.perspective_projection(45.0, self.aspect_ratio, 0.1, 1000.0, dtype='f4')
+        self.cow_scene.draw(projection, camera, time)
 
-import glfw
-
-
-def main():
-    # Initialize the library
-    if not glfw.init():
-        return
-    # Create a windowed mode window and its OpenGL context
-    window = glfw.create_window(640, 480, "Hello World", None, None)
-    if not window:
-        glfw.terminate()
-        return
-
-    # Make the window's context current
-    glfw.make_context_current(window)
-    # Loop until the user closes the window
-    while not glfw.window_should_close(window):
-        # Render here, e.g. using pyOpenGL
-
-        # Swap front and back buffers
-        glfw.swap_buffers(window)
-
-        # Poll for and process events
-        glfw.poll_events()
-
-    glfw.terminate()
-
-if __name__ == "__main__":
-    main()
+Test.run()

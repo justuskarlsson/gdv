a43e424fc1718d7cf81dbb1ba133424f780b78f6
Justus Karlsson <karlssonjustus@gmail.com>
2024-02-11 18:09:04 +0100

    Working orbit view, multiple cows. But how to separate camrea from model

--- gdv/test_viewer.py
+++ gdv/test_viewer.py
@@ -3,27 +3,38 @@ from pathlib import Path
 import numpy as np
 from pyrr import Matrix44
 import moderngl
+from gdv.camera import OrbitCameraWindow, OrbitDragCameraWindow
 
-class Test(mglw.WindowConfig):
+class Test(OrbitDragCameraWindow):
     gl_version = (3, 3)
     window_size = (1920, 1080)
     aspect_ratio = 16 / 9
     title = "My Config"
     samples = 8
     resizable = False
-    resource_dir = (Path(__file__).parent / '../data').resolve()
+    resource_dir = (Path(__file__).parent / '../resources').resolve()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.cow_scene = self.load_scene('cow/cow.obj')
-        
+        self.cow_scene = self.load_scene('meshes/cow/cow.obj')
+        self.camera.projection.update(near=0.1, far=100.0)
+        self.camera.mouse_sensitivity = 0.75
+        self.camera.zoom = 2.5
 
     def render(self, time, frametime):
         self.ctx.clear(0.0, 0.0, 0.0, 0.0)
         self.ctx.enable(moderngl.DEPTH_TEST)
-        camera = np.eye(4)
-        projection = np.eye(4)
-        # projection = Matrix44.perspective_projection(45.0, self.aspect_ratio, 0.1, 1000.0, dtype='f4')
-        self.cow_scene.draw(projection, camera, time)
+        cow_world_matrix = Matrix44.from_translation([time/10.0, 0, 0], "f4")
+        self.cow_scene.draw(
+            projection_matrix=self.camera.projection.matrix,
+            camera_matrix=self.camera.matrix @ Matrix44.from_translation([0.5, 0, 0, 0], "f4"),
+            time=time,
+        )
+
+        self.cow_scene.draw(
+            projection_matrix=self.camera.projection.matrix,
+            camera_matrix=self.camera.matrix @ Matrix44.from_translation([-0.5, 0, 0, 0], "f4"),
+            time=time,
+        )
 
 Test.run()

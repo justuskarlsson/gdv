b7a4dcffe4cb22e788c45a1bf32ebeeb33788062
Justus Karlsson <karlssonjustus@gmail.com>
2024-02-25 19:50:23 +0100

    Fixed flipped uv x axis and added programs to git

--- gdv/flat_earth.py
+++ gdv/flat_earth.py
@@ -1,7 +1,8 @@
 from pyrr import Matrix44
 import moderngl
+from pathlib import Path
 import moderngl_window
-
+from moderngl_window import resources
 
 class FlatEarth(moderngl_window.WindowConfig):
     title="Flat earth"
@@ -12,6 +13,9 @@ class FlatEarth(moderngl_window.WindowConfig):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        resources.register_dir("/data/moderngl-resources/")
+        resources.register_dir((Path(__file__).parent / '../resources').resolve())
+        
         self.quad = moderngl_window.geometry.quad_2d(
             size=(360, 180), #pos=(-50, 70)
         )
@@ -33,7 +37,7 @@ class FlatEarth(moderngl_window.WindowConfig):
         #     dtype='f4',
         # )
         model_matrix = Matrix44.identity(dtype="f4") 
-        camera_matrix = Matrix44.look_at((time, 0, -100 - time), (0, 0, 0), (0, 1, 0), dtype="f4")
+        camera_matrix = Matrix44.look_at((0, 0, -100 - time), (0, 0, 0), (0, 1, 0), dtype="f4")
         self.prog['m_proj'].write(projection_matrix)
         self.prog['m_model'].write(model_matrix)
         self.prog['m_camera'].write(camera_matrix)
